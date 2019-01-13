0d594217ec8a8d6ded937100b7a3ac0d61ac9271
Moubai <soubre@gmail.com>
2019-01-13 13:41:36 +0100

    Basic encode folder
    basic setting for Anime Encode HEVC

---
+++ nvencode.ps1
@@ -0,0 +1,13 @@
+#encoder hevc nvidia
+#complete the folder where is NvEnc
+cd "E:\- P R O G Z -\NVEncC_4.29_x64"
+$InFolder = Read-Host "what is the folder to Encode ?"
+$OutFoler = Read-Host "what is Destination folder ?"
+$Files = Get-ChildItem $InFolder
+
+ForEach($FileToEncode in $Files){
+    $InFile = "$InFolder\$FileToEncode"
+    .\NVencC64.exe -i $InFile -c h265 --cqp 25 -f --output-format mkv --audio-copy --sub-copy -o "$OutFolder\$FileToEncode"
+}
+
+pause
